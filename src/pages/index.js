6df0bd6b602ae3cfcf8c1a3e46ca826982143f21
virtualzone <heiner.beck@outlook.com>
2024-05-23 21:51:53 +0200

    WebP images

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
 import styles from './index.module.css';
-import Screenshot from '@site/static/img/screenshot.png';
+import Screenshot from '@site/static/img/screenshot.webp';
 import ReactPlayer from 'react-player';
 
 function HomepageHeader() {
