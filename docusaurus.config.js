fa024514afbc6a71798292c0b6f6d349b12870a2
Jan Jonas <1097370+janjonas@users.noreply.github.com>
2025-05-02 07:51:55 +0200

    fix: update Github link to repo instead of orga

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,58 +1,57 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer').themes.github;
-const darkCodeTheme = require('prism-react-renderer').themes.dracula;
+const lightCodeTheme = require("prism-react-renderer").themes.github;
+const darkCodeTheme = require("prism-react-renderer").themes.dracula;
 
 /** @type {import('@docusaurus/types').Config} */
 export default {
-  title: 'Seatsurfing',
-  tagline: 'Open Source solution for free seating and co-working in your organization.',
-  favicon: 'img/favicon.ico',
+  title: "Seatsurfing",
+  tagline:
+    "Open Source solution for free seating and co-working in your organization.",
+  favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: 'https://seatsurfing.app',
+  url: "https://seatsurfing.app",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'seatsurfing', // Usually your GitHub org/user name.
-  projectName: 'seatsurfing.github.io', // Usually your repo name.
+  organizationName: "seatsurfing", // Usually your GitHub org/user name.
+  projectName: "seatsurfing.github.io", // Usually your repo name.
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
         },
         blog: {
           showReadingTime: true,
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       }),
     ],
   ],
 
-  clientModules: [
-    require.resolve('./_matomo.js'),
-  ],
+  clientModules: [require.resolve("./_matomo.js")],
 
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
@@ -60,84 +59,88 @@ export default {
       // Replace with your project's social card
       // image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: '',
+        title: "",
         logo: {
-          alt: 'Seatsurfing',
-          src: 'img/seatsurfing.svg',
-          srcDark: 'img/seatsurfing_white.svg',
+          alt: "Seatsurfing",
+          src: "img/seatsurfing.svg",
+          srcDark: "img/seatsurfing_white.svg",
         },
         items: [
-          { to: '/getting-started', label: 'Getting Started', position: 'left' },
-          { to: '/pricing', label: 'Pricing', position: 'left' },
           {
-            type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
-            position: 'left',
-            label: 'Docs',
+            to: "/getting-started",
+            label: "Getting Started",
+            position: "left",
           },
-          { to: '/blog', label: 'Blog', position: 'left' },
+          { to: "/pricing", label: "Pricing", position: "left" },
           {
-            href: 'https://github.com/seatsurfing/',
-            label: 'GitHub',
-            position: 'right',
+            type: "docSidebar",
+            sidebarId: "tutorialSidebar",
+            position: "left",
+            label: "Docs",
+          },
+          { to: "/blog", label: "Blog", position: "left" },
+          {
+            href: "https://github.com/seatsurfing/seatsurfing/",
+            label: "GitHub",
+            position: "right",
           },
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         links: [
           {
-            title: 'Seatsurfing',
+            title: "Seatsurfing",
             items: [
               {
-                label: 'Getting Started',
-                to: 'getting-started/',
+                label: "Getting Started",
+                to: "getting-started/",
               },
               {
-                label: 'Documentation',
-                to: 'docs/',
+                label: "Documentation",
+                to: "docs/",
               },
               {
-                label: 'Blog',
-                to: 'blog/',
+                label: "Blog",
+                to: "blog/",
               },
             ],
           },
           {
-            title: 'Resources',
+            title: "Resources",
             items: [
               {
-                label: 'Status',
-                href: 'https://status.seatsurfing.app/',
+                label: "Status",
+                href: "https://status.seatsurfing.app/",
               },
               {
-                label: 'GitHub',
-                href: 'https://github.com/seatsurfing/',
+                label: "GitHub",
+                href: "https://github.com/seatsurfing/seatsurfing/",
               },
               {
-                label: 'Docker Hub',
-                href: 'https://hub.docker.com/u/seatsurfing',
+                label: "Docker Hub",
+                href: "https://hub.docker.com/u/seatsurfing",
               },
               {
-                label: 'Donate',
-                href: 'https://github.com/sponsors/seatsurfing',
+                label: "Donate",
+                href: "https://github.com/sponsors/seatsurfing",
               },
             ],
           },
           {
-            title: 'Legal',
+            title: "Legal",
             items: [
               {
-                label: 'Imprint',
-                to: 'imprint/',
+                label: "Imprint",
+                to: "imprint/",
               },
               {
-                label: 'Terms',
-                to: 'terms/',
+                label: "Terms",
+                to: "terms/",
               },
               {
-                label: 'Privacy Policy',
-                to: 'privacy-policy/',
+                label: "Privacy Policy",
+                to: "privacy-policy/",
               },
             ],
           },
@@ -149,4 +152,4 @@ export default {
         darkTheme: darkCodeTheme,
       },
     }),
-}
+};
