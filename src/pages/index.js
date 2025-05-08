10cb529146445dd67570881554a84f4b3ee79a07
Jan Jonas <1097370+janjonas@users.noreply.github.com>
2025-05-08 10:33:49 +0200

    feat: update title and description for homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,23 @@
-import React from 'react';
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import React from "react";
+import clsx from "clsx";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
+import HomepageFeatures from "@site/src/components/HomepageFeatures";
 
-import styles from './index.module.css';
-import Screenshot from '@site/static/img/screenshot.webp';
-import ReactPlayer from 'react-player';
+import styles from "./index.module.css";
+import Screenshot from "@site/static/img/screenshot.webp";
+import ReactPlayer from "react-player";
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <header className={clsx("hero hero--primary", styles.heroBanner)}>
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <Link
-            className="button button--primary button--lg"
-            to="/sign-up">
+          <Link className="button button--primary button--lg" to="/sign-up">
             Start for free
           </Link>
         </div>
@@ -29,11 +27,11 @@ function HomepageHeader() {
 }
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={``}
-      description="Open Source solution for desk sharing, room reservation, and co-working in your organization.">
+      title="Desk sharing, room reservation, free seating & co-working"
+      description="Desk sharing, room reservation, free seating, and co-working in your organization."
+    >
       <HomepageHeader />
       <main>
         <div className={styles.screenshotContainer}>
@@ -43,7 +41,13 @@ export default function Home() {
         <div className={styles.videoContainerWrapper}>
           <div className={styles.videoContainer}>
             <div className={styles.videoHeight}></div>
-            <ReactPlayer url='https://www.youtube.com/watch?v=WYa08A7iJNo' light={true} width='100%' height='100%' className={styles.videoPlayer} />
+            <ReactPlayer
+              url="https://www.youtube.com/watch?v=WYa08A7iJNo"
+              light={true}
+              width="100%"
+              height="100%"
+              className={styles.videoPlayer}
+            />
           </div>
         </div>
       </main>
