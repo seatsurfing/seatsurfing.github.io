91c0713b5072eea1df92f257ef23570dc979be9b
Jan Jonas <1097370+janjonas@users.noreply.github.com>
2025-05-09 10:57:47 +0200

    feat: update sign up forms

--- src/pages/sign-up-paid.js
+++ src/pages/sign-up-paid.js
@@ -2,21 +2,21 @@ import React from "react";
 import Layout from "@theme/Layout";
 import SignupForm from "../components/SignupForm";
 
-export default function SignUp() {
+export default function SignUpPaid() {
   return (
     <Layout title={`Sign Up`} description="Create a free Seatsurfing account">
       <main className="container container--fluid margin-vert--lg">
         <div className="row mdxPageWrapper_node_modules-@docusaurus-theme-classic-lib-theme-MDXPage-styles-module">
           <div className="col col--8">
             <article>
-              <h1>Sign up for Free Plan</h1>
+              <h1>Sign up for Paid Plan</h1>
               <p>
-                Start instantly with our Free Plan &ndash; no cost, no
-                commitment. It's free forever for up to 10 users 😍.
+                Start instantly with our Paid Plan &ndash; enterprise features
+                and no user limit 😍.
               </p>
               <p class="text--bold">
-                You can always upgrade to our Paid Plan after signing-up to
-                unlock unlimited users and other enterprise features 🚀.
+                After signing up, you need to upgrade to the Paid Plan in your
+                administration interface to unlock all features 🚀.
               </p>
               <SignupForm />
             </article>
